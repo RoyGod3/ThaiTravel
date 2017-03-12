61050f9bd20398c8f395cdee4cf90064aa1823f4
RaymondTseng <761409033@qq.com>
2017-03-12 14:19:51 +0800

    build search model.

--- Search/models.py
+++ Search/models.py
@@ -2,3 +2,30 @@ from django.db import models
 
 # Create your models here.
 
+class BaseSearch:
+    def __init__(self, name):
+        self.name = name
+
+    def buildInvertedIndex(self, files):
+        pass
+
+class ChineseSearch(BaseSearch):
+    def __init__(self):
+        BaseSearch.__init__(self, 'chi')
+
+    def buildInvertedIndex(self, files):
+        pass
+
+class ThaiSearch(BaseSearch):
+    def __init__(self):
+        BaseSearch.__init__(self, 'thai')
+
+    def buildInvertedIndex(self, files):
+        pass
+
+class EnglishSearch(BaseSearch):
+    def __init__(self):
+        BaseSearch.__init__(self, 'eng')
+
+    def buildInvertedIndex(self, files):
+        pass
